d0a9399a3dcdc5a46de7d0fbd78e13ad8c67db47
Samantha Lee <samanthaanne.lee28@gmail.com>
2020-05-14 22:00:15 +0800

    update

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -8,4 +8,40 @@ class TasksController < ApplicationController
     @tasks = Task.find(params[:id])
   end
 
+  def new
+    @tasks = Task.new
+  end
+
+  def create
+    @task = Task.new(task_params)
+    # Strong Params at the bottom, takes the argument of whitelisting @task = Task.new(params[:task]);
+    @task.save
+    # No need to redirect to a create page. Just back to normal page
+    redirect_to task_page_path(@task)
+  end
+
+  def edit
+    @task = Task.find(params[:id])
+  end
+
+  def update
+    @task = Task.find(params[:id])
+    @task.update(task_params) # @task.update(params[:task])
+    redirect_to task_page_path(@task)
+  end
+
+  def destroy
+    @task = Task.find(params[:id])
+    @task.destroy
+    redirect_to task_path
+  end
+
+
+  private
+
+  def task_params
+    params.require(:task).permit(:title, :details)
+  end
+
+
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,21 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   get 'tasks', to: 'tasks#index'
-  get 'tasks/:id', to: 'tasks#show', as: :task_show
 
+  get 'tasks/new', to: 'tasks#new' # Has to be above show
+  post 'tasks', to: 'tasks#create'
+
+  get 'tasks/:id', to: 'tasks#show', as: :task_page
+
+  get 'tasks/:id/edit', to: 'tasks#edit'
+  patch 'tasks/:id', to: 'tasks#update', as: :task
+
+  delete 'tasks/:id', to: 'tasks#destroy'
 end
+
+
+# <p>All Tasks <% link_to task.title, task_page_path(task.id) %> </p>
+# <!--
+# "Add a new Task" <%= link_to task.title, task_page_path(task.id) %> </p>
+#   same as: <a href="">Add a new Task </a>
+#   -->
